8fcc5d9ccad61b68e4f74b72cfd71d9359f40003
matiss jureivics <matissjurevics@gmail.com>
2022-03-20 14:37:23 +0000

    added name with animation and am about to lose my mind

--- src/App.svelte
+++ src/App.svelte
@@ -1,20 +1,22 @@
 <script lang="ts">
-  let i = 0
+  import Logo from './components/name.svelte'
 
+
+  export let x, y;
+  document.addEventListener('mousemove', e => {
+    x = e.clientX
+    y = e.clientY
+  })
 </script>
-
-<main class="snap-y h-screen w-screen overflow-scroll snap-mandatory">
-  <section class="h-screen bg-slate-900 snap-start flex flex-col justify-center items-center">
-      <a href="https://blog.saetom.xyz" class="p-3 px-8 absolute top-4 right-12 bg-fuchsia-700 rounded-3xl text-white font-semibold text-lg drop-shadow-lg hover:bg-fuchsia-500">Blog</a>
-      <p class="text-9xl font-bold text-white tracking-widest">Matiss</p>
-      <p class='text-9xl font-bold text-white tracking-widest'>Jurevics</p>
+<!-- Primary: F27A54 -->
+<!-- Secondary: A154F2 -->
+<!-- Tertiary: 6FCF97 -->
+<!--- BG: 30363D--->
+<main class="snap-y h-screen w-screen overflow-hidden cursor-none snap-mandatory">
+  <section class="relative h-screen bg-dark-bg snap-start flex flex-col justify-center items-center">
+    <a href="https://blog.saetom.xyz" class="p-3 z-20 cursor-pointer px-8 tracking-widest absolute top-4 right-12 bg-gradient-to-r from-primary to-secondary rounded-3xl text-white font-semibold text-lg drop-shadow-lg hover:bg-fuchsia-500">Blog</a>
+    <div id='cursor' style='top:{y}px; left:{x}px;transform: translate(-50%, -50%);scrolling: yes;' class='absolute rounded-full w-20 h-20 z-10 cursor-none border-2 border-white opacity-25' />
+    <Logo x={x} y={y}/>
   </section>
-  <section class="h-screen bg-slate-900 snap-start flex flex-col justify-center items-center">
-    <p class="text-9xl font-bold text-white tracking-widest">WIP</p>
-    <p class='text-9xl font-bold text-white tracking-wide'>Work In Progress</p>
-</section>
-<section class="h-screen bg-slate-900 snap-start flex flex-col justify-center items-center">
-  <p class="text-9xl font-bold text-white tracking-widest">WIP</p>
-  <p class='text-9xl font-bold text-white tracking-wide'>Work In Progress</p>
-</section>
+ 
 </main>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,9 +4,16 @@ const config = {
   theme: {
     extend: {
       fontFamily: {
-        "poppins": ['poppins', 'sans-mono']
+        poppins: ['poppins', 'sans-mono']
+      },
+      colors: {
+        'primary': '#F27A54',
+        'secondary': '#A154F2',
+        'tertiary': '#6FCF97',
+        'dark-bg': '#30363D'
       },
     },
+
   },
 
   plugins: [],
